fe59e543413655a3150f0c752adb9bc6c4f95f80
Alex Davis <alexd43@gmail.com>
2023-09-29 13:43:01 -0500

    day 5 / 100 if/else statements.

--- Day 3/concatenate.py
+++ Day 3/concatenate.py
@@ -13,7 +13,7 @@ rhyme = input("Give me a verb that rhymes with your person's name: ")
 print()
 print("There was a person called", name)
 print("Who did something cool like", thing, "at the wonderful", place, "where you'll find me", rhyme)
-```print("=== Your Song Generator ===")
+print("=== Your Song Generator ===")
 print("""You'll be asked a bunch of questions
 then we'll make you up an amazing
 song, totally copyright free 😭""")
