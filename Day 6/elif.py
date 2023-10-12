c258fe1f1dbfd88ad8b86b91399bf928af989604
Alex Davis <alex.davis@AGMB-QFVV4MQFWL.local>
2023-10-12 14:40:00 -0500

    day 6 elif statements

---
+++ Day 6/elif.py
@@ -0,0 +1,17 @@
+### Day 5 / 100
+#elif statements can be used to do add additional conditions to if statements that might also be true or trigger events.
+#can use "and" or "or" statements to also give more conditions on top of that.
+
+print("LOGIN HERE")
+print("##########")
+username = input("Username: ")
+password = input("Password: ")
+
+if username == "admin" and password == "admin":
+  print("Welcome, admin! We hope you are having a great day today!")
+elif username == "alex" and password == 'davis':
+  print("Welcome, Alex! We hope you are having a great day today!")
+elif username == "felicia" and password == 'davis':
+  print("Welcome, Felicia! We hope you are having a great day today!")
+else:
+  print("Wrong username or password!")
